032ba84c24f0739c0e6ae0cfdc67e7f9a13c8709
Bruce <recall4056@gmail.com>
2026-01-18 18:49:29 +0800

    feat: v0.0.9

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,6 +1,6 @@
 import Taro from '@tarojs/taro'
 
-export const API_DOMAIN_PREFIX = 'https://www.huiyoucloud.com'
+export const API_DOMAIN_PREFIX = 'https://huiyoucloud.com:8099'
 
 export const request = <T = any>(options: RequestOptions): Promise<Response<T>> =>
   new Promise((resolve, reject) => {
@@ -16,7 +16,13 @@ export const request = <T = any>(options: RequestOptions): Promise<Response<T>> 
       success: (res) => {
         const { data, statusCode } = res
         const { msg, errmsg } = data
-        const errorMessage = msg || errmsg?.[0]?.msg || errmsg?.[0]?.Value || errmsg || '请求错误'
+        const errorMessage = (
+          msg ||
+          errmsg?.[0]?.msg ||
+          errmsg?.[0]?.Value ||
+          errmsg ||
+          '请求错误'
+        ).toString()
         if (statusCode === 401 || statusCode === 403) {
           Taro.showToast({ title: errorMessage, icon: 'error', duration: 2000 })
           Taro.removeStorageSync('token')
@@ -34,7 +40,11 @@ export const request = <T = any>(options: RequestOptions): Promise<Response<T>> 
         }
       },
       fail: (err) => {
-        Taro.showToast({ title: err.errMsg ?? '请求错误', icon: 'error', duration: 2000 })
+        Taro.showToast({
+          title: err.errMsg ?? '请求错误',
+          icon: 'error',
+          duration: 2000
+        })
         reject(err)
       }
     })
